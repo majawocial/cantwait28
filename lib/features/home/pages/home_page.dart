45eaf4979534092f38107ec7c7b7148add34c198
Maja Wocial <majanna.wocial@gmail.com>
2022-10-14 16:12:39 +0200

    Pop add screan on save

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Can\'t Wait 🤩'),
+        title: const Text("Can't Wait 🤩"),
       ),
       body: const _HomePageBody(),
       floatingActionButton: FloatingActionButton(
